ac79c38850ad8b23a089e480c8b37c38c02bdeb3
oskarro <oskar.slyk@gmail.com>
2019-10-15 22:38:31 +0200

    Created scheme

---
+++ src/com/oskarro/ClassicCompressor.java
@@ -0,0 +1,4 @@
+package com.oskarro;
+
+public class ClassicCompressor {
+}
--- src/com/oskarro/Main.java
+++ src/com/oskarro/Main.java
@@ -3,6 +3,11 @@ package com.oskarro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if (args.length != 3) {
+            System.out.println("Wymagane są 3 argumenty: [sync/async] [folder do kompresji] [plik wyjściowy] ");
+        }
+
+
     }
 }
